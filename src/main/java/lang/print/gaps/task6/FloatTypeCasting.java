c7cc40826c6cb15b0d42fbf151fe4f79aa3d2f1f
Armine8 <armisha15@mail.ru>
2022-10-19 19:41:34 +0300

    Type Casting

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public static void roundNumber(float numberToBeRounded) {
+        int result = Math.round(numberToBeRounded);
+        System.out.print(result);
+    }
+
+    public static void main(String[] args){
+        roundNumber(1.99F);
+
     }
 }
